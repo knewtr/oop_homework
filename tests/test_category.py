004fd5cef345ac3db6081f4a9ef69c9274b20784
Nataly Veldina <221fuchs@gmail.com>
2024-10-10 11:31:43 +0500

    upd tests and .gitignore

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,27 +1,32 @@
+from tests.conftest import category_1
+
 
 def test_category(category_1):
-    assert category_1.name == 'Смартфоны'
-    assert category_1.description == ('Смартфоны, как средство не только коммуникации, '
-                                      'но и получение дополнительных функций для удобства жизни')
-    assert category_1.products == [
-            {
-                "name": "Samsung Galaxy C23 Ultra",
-                "description": "256GB, Серый цвет, 200MP камера",
-                "price": 180000.0,
-                "quantity": 5
-            },
-            {
-                "name": "Iphone 15",
-                "description": "512GB, Gray space",
-                "price": 210000.0,
-                "quantity": 8
-            },
-            {
-                "name": "Xiaomi Redmi Note 11",
-                "description": "1024GB, Синий",
-                "price": 31000.0,
-                "quantity": 14
-            }
+     assert category_1.name == 'Смартфоны'
+     assert category_1.description == ('Смартфоны, как средство не только коммуникации, '
+                                       'но и получение дополнительных функций для удобства жизни')
+     assert category_1.products == [
+             {
+                 "name": "Samsung Galaxy C23 Ultra",
+                 "description": "256GB, Серый цвет, 200MP камера",
+                 "price": 180000.0,
+                 "quantity": 5
+             },
+             {
+                 "name": "Iphone 15",
+                 "description": "512GB, Gray space",
+                 "price": 210000.0,
+                 "quantity": 8
+             },
+             {
+                 "name": "Xiaomi Redmi Note 11",
+                 "description": "1024GB, Синий",
+                 "price": 31000.0,
+                 "quantity": 14
+             }
         ]
-    assert category_1.category_count == 1
-    assert category_1.product_count == 3
+
+
+def test_category_product_count(category_1, category_2):
+    assert category_1.category_count == 2
+    assert category_2.product_count == 4
